c1229c6314fa39851ac8ae689b1108eabcab3bb0
Hiroshi123 <vrodxda@gmail.com>
2018-03-02 07:45:45 +0900

    [edit] emacs.d

---
+++ dotfile/emacs.d/elpa/bnfc-20160605.1227/bnfc-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "bnfc" "20160605.1227" "Define context-free grammars for the BNFC tool" '((emacs "24.3")) :commit "1b58df1dd0cb9b81900632fb2843a03b94f56fdb" :url "https://github.com/jmitchell/bnfc-mode" :keywords '("languages" "tools"))
