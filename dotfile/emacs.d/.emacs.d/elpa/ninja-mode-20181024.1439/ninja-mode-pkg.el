1d5f0abda186fa291c2cd90902ac586085b265ee
Hiroshi123 <vrodxda@gmail.com>
2019-04-03 07:09:02 +0900

    add some modes

---
+++ dotfile/emacs.d/.emacs.d/elpa/ninja-mode-20181024.1439/ninja-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ninja-mode" "20181024.1439" "Major mode for editing .ninja files" '((emacs "24")) :commit "99c5c2287b11c8bab05fb2a8cf89dc4856c708bc")
